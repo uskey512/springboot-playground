e63c067b7c39bf85b9c1dd385a7a374496d5c5a0
Yusuke Uehara <uskey1024@gmail.com>
2021-05-31 20:31:21 +0900

    Spring Initializerで生成

---
+++ src/test/java/com/uskey512/springsentry/SpringSentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uskey512.springsentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
